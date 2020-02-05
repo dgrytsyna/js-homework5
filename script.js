3799387026036d103bfeacbc327ea88154fcff5d
dgrytsyna <57596456+dgrytsyna@users.noreply.github.com>
2020-02-05 21:32:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let paragraphs = document.querySelectorAll("main p");
+let paragraphs2 = document.getElementsByTagName("p");
+let aside = document.querySelector("div aside");
+let aside2 = document.getElementsByTagName("aside");
+let footer = document.querySelector("footer");
+let footer2 = document.getElementsByTagName("footer");
