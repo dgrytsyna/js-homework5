69277c07a0cbcccb576ba273b2902ff840b64f83
dgrytsyna <57596456+dgrytsyna@users.noreply.github.com>
2020-02-05 21:30:34 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let paragraphs = document.querySelectorAll("main p");
-let paragraphs2 = document.getElementsByTagName("p");
-let aside = document.querySelector("div aside");
-let aside2 = document.getElementsByTagName("aside");
-let footer = document.querySelector("footer");
-let footer2 = document.getElementsByTagName("footer");
-console.log(footer, footer2);
